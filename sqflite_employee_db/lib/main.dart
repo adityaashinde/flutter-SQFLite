c68a1d3c744c61dfe74fc0beed3d154e2751593d
adityaashinde <adityashinde4580@gmail.com>
2024-03-31 10:08:04 +0530

    emplyeeDB.db created

--- sqflite_employee_db/lib/main.dart
+++ sqflite_employee_db/lib/main.dart
@@ -3,13 +3,28 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 dynamic database;
+
+class Employee {
+  final int empId;
+  final String name;
+  final double sal;
+
+  const Employee({required this.empId, required this.name, required this.sal});
+}
+
 void main() async {
   runApp(const MainApp());
 
   database = openDatabase(
-    join(
-      await getDatabasesPath(),
-    ),
+    join(await getDatabasesPath(), "employeeDB.db"),
+    version: 1,
+    onCreate: (db, version) {
+      db.execute('''CREATE TABLE Employee(
+              empId INT PRIMARY KEY,
+              name TEXT,
+              sal REAL
+        )''');
+    },
   );
 }
 
