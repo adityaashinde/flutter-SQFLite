21b317ac9763024c6a55815b8c2095eda7a8d0b5
adityaashinde <adityashinde4580@gmail.com>
2024-04-02 21:21:15 +0530

    Update main.dart

--- sqflite_player_example/lib/main.dart
+++ sqflite_player_example/lib/main.dart
@@ -3,13 +3,34 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 dynamic database;
+
+class Player {
+  final String name;
+  final int jerNo;
+  final int runs;
+  final double avg;
+
+  Player({
+    required this.name,
+    required this.jerNo,
+    required this.runs,
+    required this.avg,
+  });
+}
+
 void main() async {
   runApp(const MainApp());
 
   database = openDatabase(
-    join(
-      await getDatabasesPath(),
-    ),
+    join(await getDatabasesPath(), 'PlayerDB.db'),
+    version: 1,
+    onCreate: (db, version) {
+      db.execute('''CREATE TABLE Player(
+        name TEXT,
+        jerNo INTEGER PRIMARY KEY,
+        runs INT,
+        avg REAL)''');
+    },
   );
 }
 
