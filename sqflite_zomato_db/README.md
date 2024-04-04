f0d6a6a3d2161f5d588a357a2707bf83db660505
adityaashinde <adityashinde4580@gmail.com>
2024-04-04 08:39:46 +0530

    database created

---
+++ sqflite_zomato_db/README.md
@@ -0,0 +1,3 @@
+# sqflite_zomato_db
+
+A new Flutter project.
---
+++ sqflite_zomato_db/android/app/src/main/kotlin/com/example/sqflite_zomato_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqflite_zomato_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
