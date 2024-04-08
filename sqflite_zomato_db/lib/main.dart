8e27d318bd8c5925e08c20558467c98a695d5f18
adityaashinde <adityashinde4580@gmail.com>
2024-04-08 23:57:29 +0530

    Update data

--- sqflite_zomato_db/lib/main.dart
+++ sqflite_zomato_db/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class Zomato {
   final int orderNo;
@@ -35,7 +36,42 @@ class Zomato {
 
 dynamic database;
 
-Future<void> insertOrderData(Zomato obj) async {}
+Future<void> insertOrderData(Zomato obj) async {
+  final localDB = await database;
+
+  // The INSERT method from the database class is used to insert the data in the OrderFood table
+  // The Insert method takes 3 parameters i.e TABLE NAME, MAP OF DATA TO BE PASSED, CONFLICT ALGORITHM
+  // The CONFLICT ALGORITHM is applied when data with same primary key is inserted
+  // The Insert method requires the data in format of a map so we have written the method zomatoMap,
+  // In the Zomato class which will return the map of the data present in the given object
+
+  await localDB.insert(
+    "OrderFood",
+    obj.zomatoMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+// Update data
+
+Future<void> updateOrderData(Zomato obj) async {
+  final localDB = await database;
+
+  // The UPDATE method from the database is used to update the existing entry from the database
+  // The UPDATE method takes 4 parameters i.e
+  // 1. Table name
+  // 2. Map of updated data
+  // 3. where
+  // 4. whereArgs
+
+  await localDB.update(
+    "OrderFood",
+    obj.zomatoMap(),
+    where: 'orderNo=?',
+    whereArgs: {obj.orderNo},
+  );
+}
+
 void main() async {
   // We have commented the runApp method so we will have to call the
   // ensureInitialized() method from the WidgetsFlutterBinding class
@@ -93,4 +129,16 @@ void main() async {
       bill: 740.90);
 
   insertOrderData(order2);
+
+  // Update Data
+
+  order1 = Zomato(
+      orderNo: order1.orderNo,
+      custName: order1.custName,
+      hotelName: order1.hotelName,
+      food: "${order1.food}PaniPuri",
+      bill: order1.bill + 150.00);
+
+  updateOrderData(order1);
+  // print(await getOrderData());
 }
